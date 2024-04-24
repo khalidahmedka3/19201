050a3388d9aedc6b4a3183af7879e5bd7b5915fb
Khalid Ewees <144074802+khalidahmedka3@users.noreply.github.com>
2024-04-24 08:35:44 +0200

    Add files via upload

---
+++ DHT_11.ino
@@ -0,0 +1,81 @@
+// Declaring the DHT-11 libraries
+#include "dht.h"
+
+// Analog Pin sensor is connected to
+#define dht_apin A0 
+dht DHT;
+
+///////////Declarations/////////////
+
+// Your threshold value
+int humiditythres = 50;
+// The actuator1 pin decleration
+int fanout = 12;
+// The actuator1 pin decleration
+int piezzo = 11;
+
+///////////Declarations/////////////
+
+void setup() {
+  
+// pin type is either input or output
+  pinMode(piezzo, OUTPUT);
+  pinMode(fanout, OUTPUT);
+
+// Rate of exchanging data from sensor and seril monitor is 9600 bits
+  Serial.begin(9600);
+
+//Delay to let system boot
+  delay(500);
+
+// Declaring function of sensor on serial monitor
+  Serial.println("Humidity & temperature Mesurment:\n\n");
+  
+//Wait before accessing Sensor
+  delay(500);
+
+}
+
+void loop() {
+  
+//Start of Program
+  DHT.read11(dht_apin);
+  
+//Declaring the threshold on serial monitor
+  Serial.print("(Humidity Threshold = ");
+  Serial.print(humiditythres);
+  Serial.print("%) ");
+
+//Declaring the Calculated humidity value on serial monitor 
+  Serial.print("(Current humidity = ");
+  Serial.print(DHT.humidity);
+  Serial.println("%) ");
+
+//Declaring the Calculated tempreture value on serial monitor 
+  Serial.print("(temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.println("C) ");
+
+// Checks if it is above the threshold value
+  if (DHT.humidity > humiditythres) 
+  {
+    digitalWrite(fanout, HIGH);
+  } 
+  else
+  {
+    digitalWrite(fanout, LOW);
+  }
+
+// Checks if it is below the threshold value
+  if (DHT.humidity < humiditythres)
+  {
+  digitalWrite(piezzo, HIGH);
+  }
+  else
+  {
+  digitalWrite(piezzo, LOW);
+  }
+  
+//Wait 5 seconds before accessing sensor again.
+  delay(100);
+}
---
+++ MQ2_Sensor_code.ino
@@ -0,0 +1,76 @@
+///////////Declarations/////////////
+
+// The actuator pin decleration
+int fanin = 13;
+// The Sensor pin decleration
+int smokeA5 = A5;
+// Your threshold value
+int sensorThres = 8.00;
+
+///////////Declarations/////////////
+
+void setup() {
+  
+// pin type is either input or output
+  pinMode(fanin, OUTPUT);
+  pinMode(smokeA5, INPUT);
+
+// Rate of exchanging data from sensor and seril monitor is 9600 bits   
+  Serial.begin(9600);
+
+// Declaring function of sensor on serial monitor
+  Serial.print("CO concentration Mesurment:\n\n");
+
+// Delay acessing the sensor to decrease pressure
+  delay(500);
+}
+
+
+
+void loop() {
+
+///////////Callibration of MQ-2 sensor to convert its reading to CO values/////////////
+
+//Inserting the sensors readings to a variable
+  int analogSensor = analogRead(smokeA5);
+  
+//Determining clean air as an MQ-2 reading
+  int cleanAirBaseline = 125;
+
+//CO PPM at cleanairbaseline value  
+  float coPpm = 2.5; 
+
+// Calculate an offset from baseline (assuming higher sensor value indicates higher gas concentration)
+  float sensorOffset = analogSensor - cleanAirBaseline;
+
+// You'd need to implement a conversion based on your reference data (Data is obtained from a refrence curve for toxic gases in the MQ-2 datasheet)
+  if (sensorOffset > 0) {
+  coPpm = sensorOffset * 0.02;}
+
+///////////Callibration of MQ-2 sensor to convert its reading to CO values/////////////
+  
+
+//Declaring the threshold on serial monitor
+  Serial.print("(CO Threshold: ");
+  Serial.print(sensorThres);
+  Serial.print(".00 PPM)");
+
+//Declaring the Calculated CO value on serial monitor 
+  Serial.print(" (Estimated CO: ");
+  Serial.print(coPpm);
+  Serial.println(" PPM)");
+
+// Checks if it has reached the threshold value
+  if (coPpm > sensorThres)
+  {
+    digitalWrite(fanin, HIGH);
+  }
+  else
+  {
+    digitalWrite(fanin, LOW);
+  }
+
+// Delay reaccessing the sensor to decrease pressure
+  delay(100);
+}
+
